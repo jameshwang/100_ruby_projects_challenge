3e5d38e538d1657d2198a1e322024ec5e9bc63d5
James Hwang <james.hwang.usc@gmail.com>
2013-07-21 10:36:53 -0700

    Add spec file
    001_traffic_signal

---
+++ 001_traffic_signal/traffic_signal_spec.rb
@@ -0,0 +1,20 @@
+require_relative 'traffic_signal'
+
+describe TrafficSignal do
+  before(:each) do
+    @traffic_signal = TrafficSignal.new
+  end
+
+  it "isn't nil" do
+    @traffic_signal.should_not be_nil
+  end
+
+  it "flashes lights red-yellow-green" do
+    light_colors = %w(red yellow green)
+    watcher = SignalWatcher.new(@traffic_signal)
+    light_colors.each do |light_color|
+      expect(watcher).to receive(:update).with(light_color)
+    end
+    @traffic_signal.run
+  end
+end
