f508dff4f879c9cdf156b7bb20a7770d3fd8432c
James Hwang <james.hwang.usc@gmail.com>
2013-08-10 22:23:15 -0700

    Avdi's book finished http://objectsonrails.com/#sec-5_6

---
+++ 013_objects_on_rails/bloog/app/models/post.rb
@@ -0,0 +1,11 @@
+class Post
+  attr_accessor :blog, :title, :body
+
+  def initialize(attr = {})
+    attr.each { |k, v| send("#{k}=", v) }
+  end
+
+  def publish
+    blog.add_entry(self)
+  end
+end
---
+++ 013_objects_on_rails/bloog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloog::Application.config.secret_key_base = '1397a8e32464c1576b149f64a1d5151b376be397859b52aa5166def64810182c68f484f8105311994264e9bfaf8f91b4ecdfb8b16ca71840e6e68322ddee388a'
